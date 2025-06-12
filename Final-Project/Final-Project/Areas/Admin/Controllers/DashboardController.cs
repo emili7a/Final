15103b4d8f9f4ea0bc1bc9bda72c11614dc9c8c7
ClassTime <classtime@code.local>
2025-06-12 18:26:01 +0400

    final

---
+++ Final-Project/Final-Project/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Project.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
